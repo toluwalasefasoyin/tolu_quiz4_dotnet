5d96f02e30f2e146d35137823f816d1148f77f69
Toluwalase Fasoyin <tfasoyi2@my.centennialcollege.ca>
2026-04-07 11:41:15 -0400

    App running with database message

---
+++ quiz4_dotnet/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace quiz4_dotnet.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ quiz4_dotnet/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+Console.WriteLine("Database access has already been done on April 6, 2026 by Toluwalase");
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
